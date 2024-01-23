670e466acad7aa73ccbbcc59b32f55145ef25c65
Ategon <benjamin@barbeau.net>
2024-01-23 00:05:20 -0500

    Fix import errors

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['cjs', 'esm'], // Build for commonJS and ESmodules
+  target: 'es2020',
+  format: ['esm'], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
   sourcemap: true,
